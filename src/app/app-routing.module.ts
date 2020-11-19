a078430120da70346dd09e2ea4e11e5f7a7c1083
marianozzz <mariano.zanier@gmail.com>
2020-11-18 21:41:18 -0300

    guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,14 +7,17 @@ import {AddClientComponent } from './components/add-client/add-client.component'
 import {ViewComponent } from './components/view/view.component';
 import { AddProductComponent } from './components/add-product/add-product.component';
 import { LoginComponent } from './components/login/login.component';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'logout', component: LoginComponent, canActivate: [AuthGuard] },
   { path: 'clients', component: ClientsListComponent },
-  { path: '', component: ProductsListComponent },
+  { path: '', component: ProductsListComponent, canActivate: [AuthGuard] },
   { path: 'add', component: AddClientComponent},
-  { path: 'addproduct', component: AddProductComponent },
-  { path: 'view/:id', component: ViewComponent},
+  { path: 'addproduct', component: AddProductComponent, canActivate: [AuthGuard] },
+  { path: 'view/:id', component: ViewComponent, canActivate: [AuthGuard]},
+  { path: '**', redirectTo: "" , component: LoginComponent },
 
 ];
 
